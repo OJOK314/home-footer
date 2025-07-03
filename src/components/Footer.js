bee80d9bbd2d067c4dd773a4f3bca0d99a22131b
ojok314 <ojokalfred1996@gmail.com>
2025-07-03 12:09:56 -0700

    my frst commit

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+const Footer = () =>{
+return(
+   <footer className="footer-container">
+    <p>@2025copy Artisan Hub App. All reserved</p>
+   </footer> 
+);
+}
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () =>{
+ return(
+    <div>
+        <nav>
+                <a href="./Home">Home</a>
+                   <a href="./About">About</a>
+                      <a href="Contact">Contact</a> 
+        </nav>
+    </div>
+ );
+}
+export default Header;
