2bac4c31485daab6c0f18897fc44474b57e4a47e
ojok314 <ojokalfred1996@gmail.com>
2025-07-03 14:32:59 -0700

    footer was add into react project

--- src/App.js
+++ src/App.js
@@ -5,8 +5,28 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div>  
-    <Footer/> 
+    <div> 
+   <nav className="nav">
+    <ul className="ul">
+      <a className="a" href="mailto:kweyambapeter@gmail.com">Email</a>
+      <br/>
+      <a className="a" href="https://wa.me/0777520905?text:I%20am%20alfred">WhatsApp</a>
+      <br/>
+      <a className="a" href="">LinkedIn</a>
+      <br/>
+      <a className="a" href="">Facebook</a>
+      <br/>
+      <a className="a" href="">Twitter</a>
+      <br/>
+      <a className="a" href="tel:0781648144">phone call</a>
+      <br/>
+      <a className="a" href="sms:0777501149">Text messages</a>
+      <br/>
+    </ul>
+   </nav>
+    <Footer
+    
+    /> 
     </div>
   );
 }
