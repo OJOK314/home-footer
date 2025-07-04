417539cb14869b9df698c4df832ec76f072c7f9d
ojok314 <ojokalfred1996@gmail.com>
2025-07-04 01:37:56 -0700

    some small changes on  icons

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ function App() {
       <br/>
       <a className="a" href="https://wa.me/0777520905?text:I%20am%20alfred">WhatsApp</a>
       <br/>
-      <a className="a" href="">LinkedIn</a>
+      <a className="a" href="https://www.linkedin.com/in/ojok-alfred/">LinkedIn</a>
       <br/>
-      <a className="a" href="">Facebook</a>
+      <a className="a" href="https://www.facebook.com/" >Facebook</a>
       <br/>
-      <a className="a" href="">Twitter</a>
+      <a className="a" href="https://twitter.com/">Twitter</a>
       <br/>
-      <a className="a" href="tel:0781648144">phone call</a>
+      <a className="a" href="tel:0781648144">Phone call</a>
       <br/>
       <a className="a" href="sms:0777501149">Text messages</a>
       <br/>
